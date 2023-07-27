cc358006fcc928d6b3b856b1c6327473dc05c780
Elbert <elbertnatan@hotmail.com>
2023-07-27 12:00:23 -0300

    - criando mapa para ser utilizado para sumario

--- quiz_app/lib/results_screen.dart
+++ quiz_app/lib/results_screen.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.choosenAnswers});
 
   final List<String> choosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (int i = 0; i < choosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'answer': choosenAnswers[i],
+        'correct_answer': questions[i].answers
+      });
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
