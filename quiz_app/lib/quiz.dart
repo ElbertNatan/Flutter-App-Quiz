03ca2ec06e73548c623236cdbc771adcc75e2d7e
Elbert <elbertnatan@hotmail.com>
2023-07-27 11:30:03 -0300

    - colocando verificação de questões respondidas

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
@@ -14,7 +15,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
@@ -25,6 +26,12 @@ class _QuizState extends State<Quiz> {
 
   void addAnswer(String answer) {
     selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start-screen';
+      });
+    }
   }
 
   @override
